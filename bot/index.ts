47f2ce9da227d460ce80fbc44f02057ee20ffeed
Saghetti0 <weeb.saghetti@gmail.com>
2024-04-20 17:50:48 -0700

    big refactor, new features Transcriber now supports slash commands to configure auto-transcription, the ability to transcribe messages using context menu actions, and can be added to a user to allow for transcribing anywhere.

--- bot/index.ts
+++ bot/index.ts
@@ -1,4 +1,13 @@
-import { Client, DiscordAPIError, Events, GatewayIntentBits, Message, PermissionFlagsBits } from "discord.js";
+import { 
+  ChatInputCommandInteraction,
+  Client,
+  Events,
+  GatewayIntentBits,
+  Message,
+  MessageContextMenuCommandInteraction,
+  MessageFlags,
+  PermissionFlagsBits
+} from "discord.js";
 import configJson from "./config.json";
 import { createClient } from "celery-node";
 import process from "process";
@@ -27,6 +36,7 @@ const client = new Client({ intents: [
   GatewayIntentBits.GuildMembers,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent,
+  GatewayIntentBits.DirectMessages,
 ] });
 
 client.on(Events.ShardError, error => {
@@ -41,118 +51,216 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.MessageCreate, async (e) => {
+async function handleCommandAutotranscribe(e: ChatInputCommandInteraction) {
+  console.log("got chatinput", e);
+
+  const subcommand = e.options.getSubcommand(true);
+
+  switch (subcommand) {
+    case "on": {
+      await redis.hset(`guild.${e.guildId}.channel.${e.channelId}`, {
+        "auto_transcribe_enabled": "true"
+      });
+
+      await e.reply(":white_check_mark: Enabled auto transcribe in this channel");
+
+      break;
+    }
+
+    case "off": {
+      await redis.hset(`guild.${e.guildId}.channel.${e.channelId}`, {
+        "auto_transcribe_enabled": "false"
+      });
+
+      await e.reply(":white_check_mark: Disabled auto transcribe in this channel");
+
+      break;
+    }
+
+    default: {
+      await e.reply({
+        content: ":x: Subcommand must either be `on` or `off`.",
+        ephemeral: true,
+      });
+      break;
+    }
+  }
+}
+
+async function handleContextMenuTranscribe(e: MessageContextMenuCommandInteraction) {
+  const message = e.targetMessage;
+
+  const url = extractVoiceMessageUrl(message);
+
+  if (url === null) {
+    e.reply({
+      content: ":x: This doesn't look like a voice message.",
+      ephemeral: true
+    });
+    return;
+  }
+
+  console.log("Transcribing", message.id, "(context menu)");
+
+  const replyMessage = await e.reply({
+    content: ":writing_hand: Transcribing..",
+    ephemeral: true
+  });
+
   try {
-    if (e.guildId == null) return;
-  
-    let url: string | undefined = undefined;
-  
-    // this breaks for more than one voice message
-    // but that shouldn't be possible
+    const transcribeResult = await transcribeFromUrl(url);
 
-    // TODO: check for voice message flags rather than just the filename
-    // this code was written before the flags were documented
-    e.attachments.forEach(i => {
-      if (i.name == "voice-message.ogg") {
-        url = i.url;
-      }
-    });
-  
-    if (url === undefined) {
-      return;
-    }
-  
-    // temporarily store data in redis to allow for resume after crash
-    // TODO: actually implement this functionality...
-    await redis.hset(`transcribe.${e.id}`, {
-      message_id: e.id,
-      channel_id: e.channelId,
-      guild_id: e.guildId,
-      url: url,
-      state: "t_fast",
-      started: new Date().toISOString()
-    });
-  
-    // expire after 12h
-    await redis.expire(`transcribe.${e.id}`, 60 * 60 * 12);
-  
-    console.log("Transcribing", e.id);
+    console.log("Finished transcription for", message.id);
 
-    const messageLink = `https://discord.com/channels/${e.guildId}/${e.channelId}/${e.id}`;
-    let canReply = false;
-    const myself = e.guild?.members.me;
-
-    if (!e.channel.isDMBased() && myself) {
-      if (e.channel.permissionsFor(myself).has(PermissionFlagsBits.ReadMessageHistory)) {
-        canReply = true;
-      }
-    }
-
-    const prefix = canReply ? "" : `${messageLink}\n`;
-    let replyMessage: Message;
-
-    if (canReply) {
-      replyMessage = await e.reply({
-        content: ":writing_hand: Transcribing...",
-        failIfNotExists: true,
+    if (transcribeResult.length < 3800) {
+      await replyMessage.edit({
+        content: "```\n" + transcribeResult + "\n```\n",
         allowedMentions: {
           repliedUser: false
         }
       });
     } else {
-      replyMessage = await e.channel.send({
-        content: `${messageLink} :writing_hand: Transcribing...`,
+      await replyMessage.edit({
+        content: "Transcription attached as file",
+        files: [{
+          attachment: Buffer.from(transcribeResult, "utf-8"),
+          name: "transcription.txt"
+        }],
+        allowedMentions: {
+          repliedUser: false
+        }
       });
-    }
- 
-    try {
-      transcribeTask.applyAsync([url]).get().then(async value => {
-        await redis.hset(`transcribe.${e.id}`, {
-          state: "done",
-          result: value
-        });
-  
-        console.log("Finished transcription for", e.id);
-
-        if (value.length < 3800)
-          return await replyMessage.edit({
-            content: prefix + "```\n" + value + "\n```\n",
-            allowedMentions: {
-              repliedUser: false
-            }
-          });
-
-        await replyMessage.edit({
-          content: prefix + "Transcription attached as file",
-          files: [{
-            attachment: Buffer.from(value, "utf-8"),
-            name: "transcription.txt"
-          }],
-          allowedMentions: {
-            repliedUser: false
-          }
-        });
-      }).catch(async err => {
-        await redis.hset(`transcribe.${e.id}`, {
-          state: "t_err"
-        });
-        console.log("Failed transcription for", e.id);
-        await replyMessage.edit({
-          content: prefix + ":warning: Error transcribing: `" + err + "`",
-          allowedMentions: {
-            repliedUser: false
-          }
-        });
-      });
-  
-    } catch (err) {
-      await redis.hset(`transcribe.${e.id}`, {
-        state: "top_err"
-      });
-      replyMessage.edit(prefix + ":warning: Error transcribing: (tl) `" + err + "`");
     }
   } catch (e) {
-    console.log("Top level execption", e);
+    console.warn("Error transcribing", message.id, e);
+    replyMessage.edit({
+      content: ":warning: Error transcribing: `" + e + "`",
+      allowedMentions: {
+        repliedUser: false
+      }
+    });
+  }
+}
+
+async function handleAutoTranscribe(message: Message<true>) {
+  const url = extractVoiceMessageUrl(message);
+
+  if (url === null) return;
+
+  console.log("Transcribing", message.id, "(auto)");
+
+  const messageLink = `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
+
+  // check for permission to reply
+  let canReply = false;
+  const myself = message.guild?.members.me;
+
+  if (!message.channel.isDMBased() && myself) {
+    if (message.channel.permissionsFor(myself).has(PermissionFlagsBits.ReadMessageHistory)) {
+      canReply = true;
+    }
+  }
+
+  const prefix = canReply ? "" : `${messageLink}\n`;
+  let replyMessage: Message;
+
+  if (canReply) {
+    replyMessage = await message.reply({
+      content: ":writing_hand: Transcribing...",
+      failIfNotExists: true,
+      allowedMentions: {
+        repliedUser: false
+      }
+    });
+  } else {
+    replyMessage = await message.channel.send({
+      content: `${messageLink} :writing_hand: Transcribing...`,
+    });
+  }
+
+  try {
+    const transcribeResult = await transcribeFromUrl(url);
+
+    console.log("Finished transcription for", message.id);
+
+    if (transcribeResult.length < 3800) {
+      await replyMessage.edit({
+        content: prefix + "```\n" + transcribeResult + "\n```\n",
+        allowedMentions: {
+          repliedUser: false
+        }
+      });
+    } else {
+      await replyMessage.edit({
+        content: prefix + "Transcription attached as file",
+        files: [{
+          attachment: Buffer.from(transcribeResult, "utf-8"),
+          name: "transcription.txt"
+        }],
+        allowedMentions: {
+          repliedUser: false
+        }
+      });
+    }
+  } catch (e) {
+    console.warn("Error transcribing", message.id, e);
+    replyMessage.edit({
+      content: prefix + ":warning: Error transcribing: `" + e + "`",
+      allowedMentions: {
+        repliedUser: false
+      }
+    });
+  }
+}
+
+function transcribeFromUrl(url: string): Promise<string> {
+  return transcribeTask.applyAsync([url]).get();
+}
+
+function extractVoiceMessageUrl(message: Message): string | null {
+  if (!message.flags.has(MessageFlags.IsVoiceMessage)) return null;
+  
+  for (const [name, attachment] of message.attachments) {
+    // just return the first one we get, should be fine(??)
+    return attachment.proxyURL;
+  }
+  
+  return null;
+}
+
+client.on(Events.MessageCreate, async e => {
+  if (!e.inGuild()) return;
+
+  // check if this channel has autotranscribe enabled
+
+  const enabled = (await redis.hget(`guild.${e.guildId}.channel.${e.channelId}`, "auto_transcribe_enabled") ?? "true") === "true";
+  if (!enabled) return;
+
+  handleAutoTranscribe(e);
+});
+
+client.on(Events.InteractionCreate, e => {
+  if (e.isMessageContextMenuCommand()) {
+    if (e.commandName == "Transcribe") {
+      handleContextMenuTranscribe(e);
+      return;
+    }
+  }
+  
+  if (e.isChatInputCommand()) {
+    if (e.commandName == "autotranscribe") {
+      handleCommandAutotranscribe(e);
+      return;
+    }
+  }
+
+  console.warn("Unhandled interaction?!", e);
+
+  if (e.isRepliable()) {
+    e.reply({
+      content: ":x: An error occurred while handling this interaction.",
+      ephemeral: true,
+    });
   }
 });
 
---
+++ bot/register-commands.ts
@@ -0,0 +1,56 @@
+import { REST, Routes } from "discord.js";
+import configJson from "./config.json";
+
+const commandDefs = [
+  {
+    "name": "autotranscribe",
+    "type": 1,
+    "default_member_permissions": 16,
+    "contexts": [0],
+    "description": "Configures automatic transcription for this channel",
+    "options": [
+      {
+        "type": 1,
+        "name": "on",
+        "description": "Enable automatic transcription in this channel",
+        "options": []
+      },
+      {
+        "type": 1,
+        "name": "off",
+        "description": "Disable automatic transcription in this channel",
+        "options": []
+      }
+    ]
+  },
+  {
+    "name": "Transcribe",
+    "type": 3,
+    "integration_types": [0, 1],
+    "contexts": [0, 1, 2]
+  }
+];
+
+const rest = new REST().setToken(configJson.token);
+
+(async () => {
+  const myself = await rest.get(Routes.currentApplication());
+
+  // this is dumb idc
+
+  if (myself === null || typeof myself !== "object")
+    throw Error("expected object on /applications/@me");
+
+  if (!("id" in myself)) 
+    throw Error("expected property 'id' on /applications/@me");
+
+  const myId = myself.id;
+
+  if (typeof myId !== "string")
+    throw Error("expected property 'id' to be of type string on /applications/@me");
+
+  await rest.put(
+    Routes.applicationCommands(myId),
+    { body: commandDefs },
+  );
+})();  
